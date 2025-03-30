95afc51b9d3614cd37bd66d2ba8016b8af423be6
Sarah <dorfmans@mcmaster.ca>
2025-03-30 14:50:31 -0400

    made expert interface and a couple of experts to start

---
+++ server/src/main/ExpertManager.kt
@@ -0,0 +1,9 @@
+package server.src.main
+
+class ExpertManager {
+    val uniqueExpert : Expert = UniqueExpert()
+    val latinExpert : Expert = LatinExpert()
+    val arabicExpert : Expert = ArabicExpert()
+
+
+}
--- src/LatinExpert.kt
+++
@@ -1,8 +0,0 @@
-package src
-
-class LatinExpert : Expert {
-    override fun determineLanguage(text: String): String {
-        TODO("Not yet implemented")
-    }
-
-}
