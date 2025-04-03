9e96b4691b1cc5767f53b58e5f8df69a573bc45d
KeM <mk617235733@gmail.com>
2025-04-03 18:32:35 -0400

    Finished the Home Page

--- composeApp/src/androidMain/kotlin/languify/app/HomePage.kt
+++ composeApp/src/androidMain/kotlin/languify/app/HomePage.kt
@@ -1,4 +1,13 @@
 package languify.app
 
-class HomePage {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+class HomePage:ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page_home)
+
+    }
 }
--- composeApp/src/androidMain/kotlin/languify/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/languify/app/MainActivity.kt
@@ -2,6 +2,7 @@ package languify.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
@@ -13,9 +14,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_loading)
+        val btEnter = findViewById<ImageButton>(R.id.btEnter)
         Glide.with(this)
             .asGif()
             .load(R.drawable.animation_logo) // or URL/File
             .into(findViewById(R.id.ivAnimationLogo))
+
+        btEnter.setOnClickListener {
+            val intent = Intent(this, HomePage::class.java)
+            startActivity(intent)
+        }
     }
+
 }
