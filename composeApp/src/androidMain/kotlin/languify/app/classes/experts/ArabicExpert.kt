9bd431c114162d596b348406cc6c9329286a63bf
Gurmanjot Minhas <gurmanjotminhas@gmail.com>
2025-04-05 21:32:14 -0400

    Added the latin and arabic experts. modified the enum and added a dependency to gradle composeApp build

--- composeApp/src/androidMain/kotlin/languify/app/classes/experts/ArabicExpert.kt
+++ composeApp/src/androidMain/kotlin/languify/app/classes/experts/ArabicExpert.kt
@@ -5,9 +5,54 @@ import languify.app.classes.Text
 import languify.app.classes.databases.LanguageSyntaxDatabase
 
 
-//gurman code
+import java.io.File
+import java.util.Properties
+
+import com.detectlanguage.DetectLanguage
+
 class ArabicExpert: Expert {
     override fun guessLanguage(text: Text, syntaxDatabase : LanguageSyntaxDatabase): Language {
-        return Language.NONE
+
+        try {
+            val apiKey = loadApiKey()
+            DetectLanguage.apiKey = apiKey
+        } catch (e: Exception) {
+            return Language.NONE
+        }
+        val result = DetectLanguage.simpleDetect(text.getString())
+        val lang = Language.toLanguage(result)
+
+        try {
+            val resultNotNull: Language = lang!!
+            return checkLanguage(resultNotNull)
+        } catch (npe: NullPointerException) {
+            return Language.NONE
+        }
+    }
+
+    private fun checkLanguage(language: Language): Language {
+        if (language.equals(Language.ARABIC)) {
+            return Language.ARABIC
+        } else if (language.equals(Language.PERSIAN)) {
+            return Language.PERSIAN
+        } else if (language.equals(Language.URDU)) {
+            return Language.URDU
+        } else if (language.equals(Language.KURDISH)) {
+            return Language.KURDISH
+        } else if (language.equals(Language.PASHTO)) {
+            return Language.PASHTO
+        } else {
+            return Language.NONE
+        }
+    }
+
+    private fun loadApiKey(): String? {
+        val properties = Properties()
+        val file = File("local.properties")
+        if (file.exists()) {
+            properties.load(file.inputStream())
+            return properties.getProperty("API_KEY")
+        }
+        return null
     }
 }
